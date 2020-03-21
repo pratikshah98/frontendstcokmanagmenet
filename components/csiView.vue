16082e09753c72a2a3084c4985b5a64ac46f585d
Maulik Jadav <40969469+maulik9211@users.noreply.github.com>
2020-03-21 18:31:16 +0530

    Made individual customer's view dynamic with database (Supplier and Item's backend is not ready)

--- components/csiView.vue
+++ components/csiView.vue
@@ -1,117 +1,128 @@
 <template>
     <div class="content-wrapper">
-            <div class="content-header row">
-                <div class="content-header-left col-md-9 col-12 mb-2">
-                    <div class="row breadcrumbs-top">
-                        <div class="col-12">
-                            <h2 v-if="mode=='customer'" class="content-header-title float-left mb-0">View Customer</h2>
-                            <h2 v-else-if="mode=='supplier'" class="content-header-title float-left mb-0">View Supplier</h2>
-                            <h2 v-else class="content-header-title float-left mb-0">View Item</h2>
-                        </div>
+        <div class="content-header row">
+            <div class="content-header-left col-md-9 col-12 mb-2">
+                <div class="row breadcrumbs-top">
+                    <div class="col-12">
+                        <h2 v-if="mode=='customer'" class="content-header-title float-left mb-0">View Customer</h2>
+                        <h2 v-else-if="mode=='supplier'" class="content-header-title float-left mb-0">View Supplier</h2>
+                        <h2 v-else class="content-header-title float-left mb-0">View Item</h2>
                     </div>
                 </div>
-                
             </div>
-            <div class="content-body">
-                <!-- Form wizard with step validation section start -->
-                <section id="validation">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-content">
-                                    <div class="card-body">
-                                        <form action="#" class="steps-validation wizard-circle">
-                                            <fieldset>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label v-if="mode=='customer'">Customer Name : {{ name }}</label>
-                                                        <label v-else-if="mode=='supplier'" >Supplier Name : {{ name }}</label>
-                                                        <label v-else>Item Name : {{ name }}</label>
-                                                    </div>
-                                                    <div v-if="mode!='item'"  class="col-md-6">
-                                                        <label> Phone Number : {{ phoneNo }}</label>
-                                                    </div>
-                                                    <div v-else  class="col-md-6">
-                                                        <label> Price : {{ price }} </label>
-                                                    </div>
+            
+        </div>
+        <div class="content-body">
+            <!-- Form wizard with step validation section start -->
+            <section id="validation">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="card-body">
+                                    <form action="#" class="steps-validation wizard-circle">
+                                        <fieldset>
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <label v-if="mode=='customer'">Customer Name : {{ name }}</label>
+                                                    <label v-else-if="mode=='supplier'" >Supplier Name : {{ name }}</label>
+                                                    <label v-else>Item Name : {{ name }}</label>
+                                                </div>
+                                                <div v-if="mode!='item'"  class="col-md-4">
+                                                    <label> Phone Number : {{ phoneNo }}</label>
+                                                </div>
+                                                <div v-if="mode!='item'"  class="col-md-4">
+                                                    <label> GSTIN :  {{ gstNo }}</label>
+                                                </div>
+                                                
+                                                <div v-else  class="col-md-6">
+                                                    <label> Price : {{ price }} </label>
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div v-if="mode!='item'" class="col-md-6">
+                                                    <label >Email : {{ email }}</label>
+                                                </div>
+                                                <div v-if="mode!='item'" class="col-md-6">
+                                                    <label >Address : {{ address }}</label>
                                                 </div>
 
-                                                <div class="row">
-                                                    <div v-if="mode!='item'" class="col-md-6">
-                                                        <label >Email {{ email }}</label>
-                                                    </div>
-                                                    <div v-if="mode!='item'" class="col-md-6">
-                                                        <label >Address {{ address }}</label>
-                                                    </div>
-
-                                                    <div v-if="mode=='item'" class="col-md-6">
-                                                        <label >Description {{ description }}</label>
-                                                    </div>
-                                                    <div v-if="mode=='item'" class="col-md-6"></div>
+                                                <div v-if="mode=='item'" class="col-md-6">
+                                                    <label >Description : {{ description }}</label>
                                                 </div>
-                                            </fieldset>
-                                            <fieldset>
-                                                <div  class="row">
-                                                    <div class="col-md-9 col-4"></div>
-                                                    <div class="col-md-3 col-8" >
-                                                        <button style="float:right;" class="btn btn-primary" type="submit" @click="getDetails()">
-                                                            <i class="feather icon-edit-2"></i> Edit {{ mode | capitalize }}
-                                                        </button>
-                                                    </div>
+                                                <div v-if="mode=='item'" class="col-md-6"></div>
+                                            </div>
+                                        </fieldset>
+                                        <fieldset>
+                                            <div  class="row">
+                                                <div class="col-md-9 col-4"></div>
+                                                <div class="col-md-3 col-8" >
+                                                    <button style="float:right;" class="btn btn-primary" type="submit" @click="getDetails()">
+                                                        <i class="feather icon-edit-2"></i> Edit {{ mode | capitalize }}
+                                                    </button>
                                                 </div>
-                                            </fieldset>
+                                            </div>
+                                        </fieldset>
 
-                                            <!-- Step 3 -->
-                                        </form>
-                                    </div>
+                                        <!-- Step 3 -->
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </section>
-                <!-- Form wizard with step validation section end -->
+                </div>
+            </section>
+            <!-- Form wizard with step validation section end -->
 
-            </div>
         </div>
+    </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
     props: {
-            id: {
-                type: String,
-                required: false
-            },
-            mode:{
-                type: String,
-                required: true
-            },
-            value: {
-                type: Number,
-                required: false
-            }
+        id: {
+            type: String,
+            required: true
         },
-    data(){
-        return{
-            name:"Nostalgic Name",
-            phoneNo:"9408078677",
-            email:"email@gmail.com",
-            address:"Vejalpur, Ahmedabad",
-            price:"3,500",
-            description:"Incredible item !"
+        mode:{
+            type: String,
+            required: true
+        },
+        value: {
+            type: Number,
+            required: false
         }
     },
-    methods:{
-        getDetails(){
-            if(this.mode=='customer'){
-                    console.log("All Customers Details:- "+this.name+" "+this.mobNo+" "+this.email+" "+this.city+" "+this.address);
+    data(){
+        return{
+            name:"",
+            email:"",
+            address:"",
+            phoneNo:"",
+            gstNo:"",
+            price:"",
+            description:""
+        }
+    },
+    mounted(){
+        axios.get('http://localhost:4000/' + this.$props.mode + '/' + this.$props.id).then(Response=>{  
+            if(this.$props.mode=='customer'){
+                this.name = Response.data[0].customer_name;
+                this.email = Response.data[0].customer_emailId;
+                this.address = Response.data[0].customer_address;
+                this.phoneNo = Response.data[0].customer_phoneNo;
+                this.gstNo = Response.data[0].customer_gstno;
             }
-            else if(this.mode=='item'){
-                    console.log("All Suppliers Details:- "+this.name+" "+this.mobNo+" "+this.email+" "+this.city+" "+this.address);
+            else if(this.$props.mode=='supplier'){
+                
             }
             else{
-                    console.log("All Item Details:- "+this.name+" "+this.price+" "+this.description);
+                
             }
-        }
+        });
     },
     filters: {
         capitalize: function (value) {
